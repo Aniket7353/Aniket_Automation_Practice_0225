1ce3e8ef42e84b0e121b15c2f7020de802e5241b
diptinikam1998@gmail.com <diptinikam1998@gmail.com>
2025-02-18 12:05:19 +0530

    interview

--- src/ex_Ternary_Operator/Lab_001_Interview_Que.java
+++ src/ex_Ternary_Operator/Lab_001_Interview_Que.java
@@ -5,6 +5,6 @@ public class Lab_001_Interview_Que {
         int score=90;
 
         String result= (score>=90 && score<=100) ? "Grade A" : (score>=80 && score <=89 ) ? "Grade B": (score>=70 && score <=79 ) ? "Grade C":  (score>=60 && score <=69 ) ? "Grade D":  (score>=0 && score <=59 ) ? "Grade F": "Invalid Score" ;
-        System.out.println("Grade of the student === " + result);
+        System.out.println("Grade of the student = " + result);
     }
 }
