30e5400047496496fa857f63a2fe252856452eb6
diptinikam1998@gmail.com <diptinikam1998@gmail.com>
2025-02-18 17:41:42 +0530

    Cummit1

---
+++ src/ex_Ternary_Operator/Lab_013_Task11Table.java
@@ -0,0 +1,27 @@
+package ex_Ternary_Operator;
+
+public class Lab_013_Task11Table {
+    public static void main(String[] args) {
+        int n= Integer.parseInt(args[0]);
+        System.out.printf("%dx1 = %d",n,n*1);
+        System.out.println();
+        System.out.printf("%dx2 = %d",n,n*2);
+        System.out.println();
+        System.out.printf("%dx3 = %d",n,n*3);
+        System.out.println();
+        System.out.printf("%dx4 = %d",n,n*4);
+        System.out.println();
+        System.out.printf("%dx5 = %d",n,n*5);
+        System.out.println();
+        System.out.printf("%dx6 = %d",n,n*6);
+        System.out.println();
+        System.out.printf("%dx7 = %d",n,n*7);
+        System.out.println();
+        System.out.printf("%dx8 = %d",n,n*8);
+        System.out.println();
+        System.out.printf("%dx9 = %d",n,n*9);
+        System.out.println();
+        System.out.printf("%dx10 = %d",n,n*10);
+
+    }
+}
