2791f998117150b274070482a28c23d5c29a41d4
diptinikam1998@gmail.com <diptinikam1998@gmail.com>
2025-03-02 14:31:04 +0530

    Commit

---
+++ src/ex_07_StringFunctions/Lab_004_StringFunctions.java
@@ -0,0 +1,9 @@
+package ex_07_StringFunctions;
+
+public class Lab_004_StringFunctions {
+    public static void main(String[] args) {
+        String a= "Andy";
+        System.out.println(a.length());
+        System.out.println(a.charAt(2));
+    }
+}
